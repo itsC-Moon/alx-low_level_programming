759e7c1c160df5d0f07da3a78b03f84cde4b84b3
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-03-21 14:59:48 +0000

    h

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - checks if character is a letter
+ *            both lowercase or uppercase
+ *
+ * @n: takes input from other functions.
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		for (int i = 0; i <= n; ++i)
+		{
+			for (int j = 0; j <= n; ++j)
+			{
+				printf("%d", i * j);
+				if (j + 1 <= n)
+				{
+					printf(",");
+					if (i * (j + 1) <= 9)
+						printf("   ");
+					else if (i * (j + 1) < 100)
+						printf("  ");
+					else
+						printf(" ");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+					if( i * (j+1) < 9)
+						printf("   ");
+					else if (i * (j+1) < 100)
+						printf("  ");
+					else
+						printf(" ");
