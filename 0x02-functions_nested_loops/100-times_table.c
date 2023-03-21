fa43c47bcbe7d516fbb1a902ae8f1fd9450135cf
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-03-21 15:03:12 +0000

    h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,14 @@
  */
 void print_times_table(int n)
 {
+	int i;
+	int j;
+
 	if (n >= 0 && n <= 15)
 	{
-		for (int i = 0; i <= n; ++i)
+		for (i = 0; i <= n; ++i)
 		{
-			for (int j = 0; j <= n; ++j)
+			for (j = 0; j <= n; ++j)
 			{
 				printf("%d", i * j);
 				if (j + 1 <= n)
--- 0x02-functions_nested_loops/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-					if( i * (j+1) < 9)
-						printf("   ");
-					else if (i * (j+1) < 100)
-						printf("  ");
-					else
-						printf(" ");
