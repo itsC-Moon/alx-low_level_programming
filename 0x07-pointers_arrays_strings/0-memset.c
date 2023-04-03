7154ea9b8701e61343827728c4127d07ff0beb98
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-03 18:00:39 +0000

    e

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,20 +13,13 @@
  *
  * Return: A pointer to the filled memory
  *         area @s
-*/
-
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * declare an unsigned int
-	 * because we are storing a
-	 * value that will always be
-	 * non-negative (zero or positive)
-	*/
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
+		*(s + i) = b;
 
 	return (s);
 }
