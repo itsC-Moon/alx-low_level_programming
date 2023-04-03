9b850aa9719e9509b1e7d687cbeadff4a12c0fa2
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-03 17:56:04 +0000

    f

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,16 +1,9 @@
-/**
- * set_string - a function that fills
- *         memory with a constant byte
- *
- * @s: input pointer to char type
- *    represents the pointer to the
- * @to: input variable of char type
- *    represents the character to
- *    fill s alx in3al mok tasila
- *
- * Return: void
- */
 #include "main.h"
+/**
+ * set_string - sets pointer val to char
+ * @s: pointer
+ * @to: char
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;
