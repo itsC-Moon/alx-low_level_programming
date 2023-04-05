292278cf083fa50249e6eeedb028fe3de3031919
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-05 15:54:44 +0000

    h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,14 @@
  * @s: the string
  *
  * Return: nothing
-*/
+ */
 
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,26 +6,13 @@
  * @s: string
  *
  * Return: nothing
-*/
+ */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		putchar(*s);
 	}
-	
-}
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _print_rev_recursion("\nColton Walker");
-    return (0);
 }
