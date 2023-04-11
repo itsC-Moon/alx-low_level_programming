8a68e0c5edaf3f5f101c2de83bb6b67725fb1b44
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-11 15:36:54 +0000

    hello from bash

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,30 @@
 #include <stdlib.h>
+
 /**
- * create_array - create and set buffer
- * @c: char to set to
- * @size: the size of of buffer
+ * create_array - creates an array of chars and initializes
+ *                it with a specific char.
  *
- * Return: Nothing.
- */
+ * @size: array size
+ * @c: char to initialize array with
+ *
+ * Return: array @a
+*/
+
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
 	unsigned int i;
+	char *a;
 
 	if (size == 0)
 		return (NULL);
 
-	buffer = (char *)malloc(sizeof(char) * size);
-	if (!buffer)
+	a = malloc(size * sizeof(char));
+
+	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; ++i)
-		buffer[i] = c;
+	for (i = 0; i < size; i++)
+		a[i] = c;
 
-	return (buffer);
+	return (a);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - a function that duplicates a string
+ *
+ * @str: input string to duplicate
+ *
+ * Return: NULL if str == NULL
+ *         @str
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *s;
+
+	while (str[i] != '\0')
+		i++;
+
+	s = (char *)malloc(sizeof(char) * i + 1);
+	if (!s)
+		return (NULL);
+
+	i = 0;
+
+	while (str[i])
+		s[i++] = str[i];
+	s[i] = '\0';
+
+	return (s);
+}
