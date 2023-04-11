76c601d9730cd3626e6273f08c8986c2504b2583
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-11 21:31:26 +0000

    hello from bash

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,39 +7,43 @@
  * @s2: input to string 2
  *
  * Return: NULL on faliure
- */
+*/
 
 char *str_concat(char *s1, char *s2)
 {
-	int lena = 0;
-	int lenb = 0;
-	int len = 0;
-	char *str;
+	int a = 0, b = 0;
+	int i, j;
+	char *s;
 
-	if (!s1 || !s2)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	/*find length of str1 & str2*/
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+
+	/*+1 for our end of string character*/
+	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
+
+	if (s == NULL)
 		return (NULL);
 
-	while (s1[lena])
-		lena++;
-
-	while (s2[lenb])
-		lenb++;
-
-	str = (char *)malloc(sizeof(char) * (lena + lenb + 1));
-	if (str == NULL)
-		return (NULL);
-	while (s1[len])
+	/*add the first string to array s*/
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	/*add the second string to array s*/
+	for (j = 0; s2[j] != '\0'; j++)
 	{
-		str[len] = s1[len];
-		len++;
+		s[i] = s2[j];
+		i++;
 	}
-	lenb = 0;
-	while (s2[lenb])
-	{
-		str[len] = s2[lenb];
-		lenb++;
-		len++;
-	}
-	str[len] = '\0';
-	return (str);
+
+	/*null terminate our new string*/
+	s[i] = '\0';
+
+	return (s);
 }
