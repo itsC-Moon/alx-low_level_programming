fca2ce93a18334f893e5139368c9e3a2f82f3ac3
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-20 09:48:51 +0000

    h

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,8 +8,26 @@
  * @n: start of input variables
  *
  * Return: nothing
-*/
+ */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list ap;
+	int i = 0;
+	char *str;
+
+	va_start(ap, n);
+	while (i < (int)n)
+	{
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < (int)n - 1 && separator)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
