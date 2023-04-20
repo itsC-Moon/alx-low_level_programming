5df71ca4bd45ff3b5e03aeec48b5a32bfde47cb0
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-20 09:39:50 +0000

    its good

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,9 +8,21 @@
  * @n: start of input variables
  *
  * Return: nothing
-*/
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list ap;
+	int i = 0;
 
+	va_start(ap, n);
+	while (i < (int)n)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < (int)n - 1)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
