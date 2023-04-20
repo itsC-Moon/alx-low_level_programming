a4fa8cf798f05ce1bb7e79d008bb200377d5f168
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-20 10:08:03 +0000

    h

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,74 +1,4 @@
 #include "variadic_functions.h"
-
-/**
- * print_char - print a char
- *
- * @arg: a list of argument pointing
- *      to the character to be printed
- *
- * Return: nothing
-*/
-
-void print_char(va_list arg)
-{
-	char c = va_arg(arg, int);
-
-	printf("%c", c);
-}
-
-/**
- * print_int - print an integer
- *
- * @arg: a list of argument pointing
- *      to the character to be printed
- *
- * Return: nothing
-*/
-
-void print_int(va_list arg)
-{
-	int n = va_arg(arg, int);
-
-	printf("%d", n);
-}
-
-/**
- * print_float - print a float
- *
- * @arg: a list of argument pointing
- *      to the character to be printed
- *
- * Return: nothing
-*/
-
-void print_float(va_list arg)
-{
-	float n = va_arg(arg, double);
-
-	printf("%f", n);
-}
-
-/**
- * print_string - print a string
- *
- * @arg: a list of argument pointing
- *      to the character to be printed
- *
- * Return: nothing
-*/
-
-void print_string(va_list arg)
-{
-	char *str = va_arg(arg, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
 /**
  * print_all - a function that prints anything
  *
@@ -79,9 +9,48 @@ void print_string(va_list arg)
  *              int, float or char * is ignored
  *
  * Return: nothing
-*/
+ */
 
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
+	va_list ap;
+	int i = 0;
+	char *str;
+	int flag = 0;
 
+	va_start(ap, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 's':
+			str = va_arg(ap, char *);
+			if (str)
+				printf("%s", str);
+			else
+				printf("(nil)");
+			flag = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			flag = 1;
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(ap, double));
+			flag = 1;
+			break;
+		case 'c':
+			printf("%c", (char)va_arg(ap, int));
+			flag = 1;
+			break;
+		}
+		if (flag && format[i + 1])
+		{
+			printf(", ");
+			flag = 0;
+		}
+		i++;
+	}
+	printf("\n");
 }
+#include "variadic_functions.h"
