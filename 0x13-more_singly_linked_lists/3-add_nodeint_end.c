705e5e2224abba90a6f0fba981bf7e34e71d3022
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-05-02 12:42:57 +0100

    this not me

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
  * @n: element int to add to new node
  *
  * Return: address of the new element or NULL if it fails
-*/
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *temp;
@@ -22,16 +22,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	temp = *head;
 
-	/*place new_node first if head is NULL*/
 	if (temp == NULL)
 		*head = new_node;
 	else
 	{
-		/*if head is not null, and next is not NULL*/
 		while (temp->next != NULL)
-			temp = temp->next;	/*go to the last node*/
+			temp = temp->next;
 
-		/*place our new node at the end*/
 		temp->next = new_node;
 	}
 
