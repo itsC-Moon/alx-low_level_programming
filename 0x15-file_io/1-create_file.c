a17b47dde408a7cfbf5d84d52b77ec39d6d185f3
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-05-08 18:47:48 +0100

    spoiler jujutsu kaisen... hhhh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - a function that creates a file
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
- */
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success OR -1 on faliure
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd_open, fd_write, len = 0;
+	int file, write_status, words = 0;
 
-	if (!filename)
+	if (filename == NULL) /*check if filename is present*/
 		return (-1);
 
-	if (!text_content)
+	/*open file by creating it and if it exists write but truncate to 0*/
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /*check if file creation was a success*/
+		return (-1);
+
+	if (text_content) /*write content to file if its not NULL*/
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
+
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a success*/
+			return (-1);
 	}
 
-	fd_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	fd_write = write(fd_open, text_content, len);
-
-	if (fd_open == -1 || fd_write == -1)
-		return (-1);
-
-	close(fd_open);
-
+	close(file); /*close file*/
 	return (1);
 }
